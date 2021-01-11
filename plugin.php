b1166744e733a44179817316a2154a87318db240
saber13812002 <saber.tabatabaee@gmail.com>
2021-01-11 23:13:41 +0330

    start to develop my new plugun with in the name of Allah

---
+++ plugin.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Plugin Name: Islam Daily Hadith (The Prophet Muhammad (SMAWA) and all 12 Imam Speech)
+ * Plugin URI: https://islamplus.net
+ * Description: This Plugin Developed for Free To showing Daily Shia Hadith On Wordpress Website
+ * Version: 0.0.1
+ * Author: Saber tabatabaee yazdi
+ * Author URI: https://github.com/saber13812002
+ */
